20e792f788885c43269a2b786b6154d23854be4c
Boris <cherkasskiy@gmail.com>
2017-01-22 22:54:17 -0700

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using NAudio;
+using System.Threading;
+using System.IO;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() == 0)
+            {
+                Console.WriteLine("Basic text to speach converter");
+                Console.WriteLine("Expecting text in the command line parameter");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Creating Amazon Polly client");
+            Amazon.Polly.AmazonPollyClient cl = new Amazon.Polly.AmazonPollyClient(RegionEndpoint.USWest2);
+            Amazon.Polly.Model.SynthesizeSpeechRequest req = new Amazon.Polly.Model.SynthesizeSpeechRequest();
+            req.Text = String.Join(" ", args);
+            req.VoiceId = Amazon.Polly.VoiceId.Salli;
+            req.OutputFormat = Amazon.Polly.OutputFormat.Mp3;
+            req.SampleRate = "8000";
+            req.TextType = Amazon.Polly.TextType.Text;
+
+            Console.WriteLine("Sending Amazon Polly request: " + req.Text);
+            Amazon.Polly.Model.SynthesizeSpeechResponse resp = cl.SynthesizeSpeech(req);
+
+            MemoryStream local_stream = new MemoryStream();
+            resp.AudioStream.CopyTo(local_stream);            
+            local_stream.Position = 0;
+            Console.WriteLine("Got mp3 stream, lenght: " + local_stream.Length.ToString());
+
+            NAudio.Wave.Mp3FileReader reader = new NAudio.Wave.Mp3FileReader(local_stream);
+            NAudio.Wave.WaveStream wave_stream = NAudio.Wave.WaveFormatConversionStream.CreatePcmStream(reader);
+            NAudio.Wave.BlockAlignReductionStream ba_stream = new NAudio.Wave.BlockAlignReductionStream(wave_stream);
+            NAudio.Wave.WaveOut wout = new NAudio.Wave.WaveOut();
+
+            Console.Write("Playing stream...");
+            wout.Init(ba_stream);
+            wout.Play();
+            while (wout.PlaybackState == NAudio.Wave.PlaybackState.Playing)
+            {
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("..Done");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Amazon Polly Tester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Amazon Polly Tester")]
+[assembly: AssemblyCopyright("Copyright Boris ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("951a5be3-3cd5-4b34-bfe4-4fb527d7bee2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
